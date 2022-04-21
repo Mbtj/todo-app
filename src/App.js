48ed806fb8f588c116e57d8de7d1ca0cc4715aed
TJ <michael.btj@hotmail.com>
2022-04-20 23:20:45 -0400

    update component import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoForm';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <h1>
         Todo App
       </h1>
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
