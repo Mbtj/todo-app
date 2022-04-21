fe30e6f48ddb2a82bb4f213cad2286deee8c2412
TJ <michael.btj@hotmail.com>
2022-04-20 23:08:54 -0400

    add form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,7 +3,14 @@ import React, { useState } from 'react'
 function TodoForm() {
   const [input, setInput] = useState();
   return (
-    <div>TodoForm</div>
+    <form className='todo-form'>
+      <input
+        type="text"
+        placeholder='Add a todo'
+        value={input}
+        name='text'
+        className='todo-input' />
+    </form>
   )
 }
 
