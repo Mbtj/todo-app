dfbc43e4d22010afaab048c18378d33a32b8109d
TJ <michael.btj@hotmail.com>
2022-04-20 23:35:04 -0400

    add state

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,7 @@ function TodoList() {
   const [todos, setTodos] = useState([]);
   return (
     <div>
-      <h1>What are we doing today?</h1>
+      <h1>What are we doing today??</h1>
       <TodoForm />
     </div>
   )
